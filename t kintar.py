38d3b33aee92534cf457c3c491b572f65c1ae8f9
gameybobcat622 <79866297+gameybobcat622@users.noreply.github.com>
2021-05-11 11:03:38 -0600

    Update t kintar.py

--- t kintar.py
+++ t kintar.py
@@ -23,33 +23,56 @@ def mainMenu():
     clerwindow()
     LMain = Label(top, text = "Block 5 Gui Projects")
     LMain.grid(column = 0, row = 1)
-    B1Main = button(text= "Week 1", bg= "white")
+    B1Main = button(text= "Week 1", bg= "white", comand = week1)
     B1Main.grid(column= 0, row = 2)
     B2Main = button(text = "Week 2", bg = "White")
     B2Main.grid(column= 0, row = 3)
     B3Main = button(text = "Week 3", bg = "White")
     B3Main.grid(column= 0, row = 4)
-                
-#this is a label widget
-L1 = Label(top, text="Playlist generator")
-L1.grid(column= 0, row= 1)
+def week1():
+    clearWindow()
+    def results():
+        result = E1.get()
+        playlist.append(result)
+        E1.delete(0, END)
+    
+    #this is a label widget
+    L1 = Label(top, text="Playlist generator")
+    L1.grid(column= 0, row= 1)
 
-#this is a entry widget
-E1 = Entry(top, bd = 5)
-E1.grid(column= 0, row= 2)
+    #this is a entry widget
+    E1 = Entry(top, bd = 5)
+    E1.grid(column= 0, row= 2)
 
 
-#This is a button widget
-B1 = Button(text = " + ", bg="red", command= results)
-B1.grid(column= 1, row= 3)
+    #This is a button widget
+    B1 = Button(text = " + ", bg="red", command= results)
+    B1.grid(column= 1, row= 3)
 
-B2 = Button(text = " Print ", bg = "light blue", command = printList)
-B2.grid(column= 2, row= 2)
+    B2 = Button(text = " Print ", bg = "light blue", command = printList)
+    B2.grid(column= 2, row= 2)
 
-B3 = Button(text = "Export List", bg = "#B4FFCD", command = results)
-B3.grid(column = 0, row = 3)
+    B3 = Button(text = "Export List", bg = "#B4FFCD", command = results)
+    B3.grid(column = 0, row = 3)
 
-Bexit = button(text = "Clear Window", bg = "red", command = clearWindow)
-Bexit.grid(column = 1, row = 3)
-
+    Bexit = button(text = "Main menu", bg = "red", command = mainMenu)
+    Bexit.grid(column = 1, row = 3)
+    
+    def week2():
+        clearWindow()
+        B1Week2 = Button()
+        L1Week2 = Label()
+        L2Week2 = Label()
+        L3Week2 = Label()
+        E1Week2 = Entry()
+        E2Week2 = Entry()
+    
+    
+    
+    
+    
+    
+    
+if__name__ == "__main__":
+    mainMenue()
 top.mainloop()

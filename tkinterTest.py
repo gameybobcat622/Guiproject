e134d4ed05622f0e7bd0a9d2ddd3ac747f9a5f45
gameybobcat622 <79866297+gameybobcat622@users.noreply.github.com>
2021-05-20 14:05:58 -0600

    Add files via upload

--- tkinterTest.py
+++ tkinterTest.py
@@ -1,11 +1,20 @@
 from tkinter import  *
+import random
 
-top = Tk()
+
 playlist = []
 
-top = Tk()
+
 playlist = []
 myRolls = []
+
+top = Tk(className='Python Examples - Window Color')
+# set window size
+top.geometry("400x200")
+
+#set window color
+top['background']='light blue'
+#gui.configure(background='yellow')
 
 def results():
     result = E1.get()
@@ -20,26 +29,40 @@ def exportList():
         for item in list:
             f.write("%s/n" % item)
 
-def clearwindow():
+def ClearWindow():
     for widget in top.winfo_children():
-        widgets.destroy()
+        widget.destroy()
 def mainMenu():
-    clerwindow()
+    ClearWindow()
     LMain = Label(top, text = "Block 5 Gui Projects")
     LMain.grid(column = 0, row = 1)
-    B1Main = button(text= "Week 1", bg= "white", comand = week1)
+    B1Main = Button(text= "Week 1", bg= "white", command = week1)
     B1Main.grid(column= 0, row = 2)
-    B2Main = button(text = "Week 2", bg = "White",command = week2)
+    B2Main = Button(text = "Week 2", bg = "White",command = week2)
     B2Main.grid(column= 0, row = 3)
-    B3Main = button(text = "Week 3", bg = "White")
+    B3Main = Button(text = "Week 3", bg = "White")
     B3Main.grid(column= 0, row = 4)
 def week1():
-    clearWindow()
+    ClearWindow()
     def results():
         result = E1.get()
         playlist.append(result)
         E1.delete(0, END)
-    
+   
+    """
+    if int(chosen_element) == 1:
+        print('Adding a Customer!!!')
+    elif int(chosen_element) == 2:
+        print('Searching a Customer!!!')
+    elif int(chosen_element) == 3:
+        print('Update Customers!!!')
+    elif int(chosen_element) == 4:
+        print('Delete a Customer!!!')
+    elif int(chosen_element) == 5:
+        sys.exit()
+    else:
+        print('Sorry, the value entered must be a number from 1 to 5, then try again!')
+     """   
     #this is a label widget
     L1 = Label(top, text="Playlist generator")
     L1.grid(column= 0, row= 1)
@@ -59,30 +82,30 @@ def week1():
     B3 = Button(text = "Export List", bg = "#B4FFCD", command = results)
     B3.grid(column = 0, row = 3)
 
-    Bexit = button(text = "Main menu", bg = "red", command = mainMenu)
-    Bexit.grid(column = 1, row = 3)
+    Bexit = Button(text = "Main menu", bg = "red", command = mainMenu)
+    Bexit.grid(column = 1, row = 4)
     
-    def week2():
+def week2():
         def rollDice():
             dieType = E2Week2.get()
             rollTimes = E1Week2.get()
             #clear window after updateing veriables 
-            clearwindow()
+            ClearWindow()
             for x in range(0, int(rollTimes)):
                 myRolls.append(random.randint(1, int(dieType)))
             #build the results window
-            L4Week2 = Lable(top, text = "Here's your rolls!")
+            L4Week2 = Label(top, text = "Here's your rolls!")
             L4Week2.grid(column= 0, row= 1)
             
-            L5Week2 = Lable(top, text= "{}".format(myRolls))
+            L5Week2 = Label(top, text= "{}".format(myRolls))
             L5Week2.grid(column= 0, row=2)
             
-            B2Week2 = Botton(text="Main Menu", bg= "yellow", command= mainMenu)
+            B2Week2 = Button(text="Main Menu", bg= "yellow", command= mainMenu)
             B2Week2.grid(column= 0, row=3)
 
         
-        clearWindow()
-        B1Week2 = Button(text= "Roll em!" , bg="yellow")
+        ClearWindow()
+        B1Week2 = Button(text= "Roll em!" , bg="yellow", command = rollDice)
         B1Week2.grid(column = 2, row= 4)
         
         L1Week2 = Label(top, text= "Dice Roller App")
@@ -94,10 +117,10 @@ def week1():
         L3Week2 = Label(top, text= "# of Slides")
         L3Week2.grid(column= 3, row= 2)
         
-        E1Week2 = Entry(Top, bd = 5)
+        E1Week2 = Entry(top, bd = 5)
         E1Week2.grid(column= 0, row = 3)
 
-        E2Week2 = Entry(Top, bd = 5)
+        E2Week2 = Entry(top, bd = 5)
         E2Week2.grid(column= 3, row = 3)
 
 
@@ -105,12 +128,11 @@ def week1():
 top.geometry('200x100')
 	
 # Create a Button
-button = Button(top,
-				text = 'Submit',
-				bg='blue').pack()
+button = Button(top, text = 'Submit', bg='blue').pack()
 
 
 
 if __name__ == "__main__":
-    mainMenue()
+    mainMenu()
     top.mainloop()
+    
